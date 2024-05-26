e650fab0a4229a6c31802338bfbbee1753164892
Shole <navatejapendiala@Shole.local>
2024-05-26 13:42:37 +0530

    code with chatbot

--- server/server.py
+++ server/server.py
@@ -6,9 +6,7 @@ from langchain_openai import OpenAIEmbeddings
 from langchain.prompts import PromptTemplate
 from langchain_community.chat_models import ChatOpenAI
 from langchain.chains import LLMChain
-from dotenv import load_dotenv
 
-load_dotenv()
 
 app = Flask(__name__)
 
